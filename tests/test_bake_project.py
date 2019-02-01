5f856d0cb7c50e55b88b106b4fc97aa3c06384b2
Kylen Solvik <kysolvik@gmail.com>
2019-02-01 16:46:02 -0700

    Deleted commented out tests. Can get from pypackage later if needed

--- tests/test_bake_project.py
+++ tests/test_bake_project.py
@@ -108,45 +108,12 @@ def test_bake_with_apostrophe_and_run_tests(cookies):
         run_inside_dir('python setup.py test', str(result.project)) == 0
 
 
-# def test_bake_and_run_travis_pypi_setup(cookies):
-#     # given:
-#     with bake_in_temp_dir(cookies) as result:
-#         project_path = str(result.project)
-#
-#         # when:
-#         travis_setup_cmd = ('python travis_pypi_setup.py'
-#                             ' --repo audreyr/cookiecutter-pypackage --password invalidpass')
-#         run_inside_dir(travis_setup_cmd, project_path)
-#         # then:
-#         result_travis_config = yaml.load(result.project.join(".travis.yml").open())
-#         min_size_of_encrypted_password = 50
-#         assert len(result_travis_config["deploy"]["password"]["secure"]) > min_size_of_encrypted_password
-
-
 def test_bake_without_travis_pypi_setup(cookies):
     with bake_in_temp_dir(cookies, extra_context={'use_pypi_deployment_with_travis': 'n'}) as result:
         result_travis_config = yaml.load(result.project.join(".travis.yml").open())
         assert "deploy" not in result_travis_config
         assert "python" == result_travis_config["language"]
         found_toplevel_files = [f.basename for f in result.project.listdir()]
-
-
-# def test_bake_without_author_file(cookies):
-#     with bake_in_temp_dir(cookies, extra_context={'create_author_file': 'n'}) as result:
-#         found_toplevel_files = [f.basename for f in result.project.listdir()]
-#         assert 'AUTHORS.rst' not in found_toplevel_files
-#         doc_files = [f.basename for f in result.project.join('docs').listdir()]
-#         assert 'authors.rst' not in doc_files
-#
-#         # Assert there are no spaces in the toc tree
-#         docs_index_path = result.project.join('docs/index.rst')
-#         with open(str(docs_index_path)) as index_file:
-#             assert 'contributing\n   history' in index_file.read()
-#
-#         # Check that
-#         manifest_path = result.project.join('MANIFEST.in')
-#         with open(str(manifest_path)) as manifest_file:
-#             assert 'AUTHORS.rst' not in manifest_file.read()
 
 
 def test_make_help(cookies):
@@ -169,14 +136,6 @@ def test_bake_selecting_license(cookies):
             assert license in result.project.join('setup.py').read()
 
 
-# def test_bake_not_open_source(cookies):
-#     with bake_in_temp_dir(cookies, extra_context={'open_source_license': 'Not open source'}) as result:
-#         found_toplevel_files = [f.basename for f in result.project.listdir()]
-#         assert 'setup.py' in found_toplevel_files
-#         assert 'LICENSE' not in found_toplevel_files
-#         assert 'License' not in result.project.join('README.rst').read()
-
-
 def test_using_pytest(cookies):
     with bake_in_temp_dir(cookies) as result:
         assert result.project.isdir()
@@ -189,75 +148,3 @@ def test_using_pytest(cookies):
         run_inside_dir('python setup.py test', str(result.project)) == 0
 
 
-# def test_not_using_pytest(cookies):
-#     with bake_in_temp_dir(cookies) as result:
-#         assert result.project.isdir()
-#         test_file_path = result.project.join('tests/test_python_boilerplate.py')
-#         lines = test_file_path.readlines()
-#         assert "import unittest" in ''.join(lines)
-#         assert "import pytest" not in ''.join(lines)
-
-
-# def test_project_with_hyphen_in_module_name(cookies):
-#     result = cookies.bake(extra_context={'project_name': 'something-with-a-dash'})
-#     assert result.project is not None
-#     project_path = str(result.project)
-#
-#     # when:
-#     travis_setup_cmd = ('python travis_pypi_setup.py'
-#                         ' --repo audreyr/cookiecutter-pypackage --password invalidpass')
-#     run_inside_dir(travis_setup_cmd, project_path)
-#
-#     # then:
-#     result_travis_config = yaml.load(open(os.path.join(project_path, ".travis.yml")))
-#     assert "secure" in result_travis_config["deploy"]["password"],\
-#         "missing password config in .travis.yml"
-
-
-# def test_bake_with_no_console_script(cookies):
-#     context = {'command_line_interface': "No command-line interface"}
-#     result = cookies.bake(extra_context=context)
-#     project_path, project_slug, project_dir = project_info(result)
-#     found_project_files = os.listdir(project_dir)
-#     assert "cli.py" not in found_project_files
-#
-#     setup_path = os.path.join(project_path, 'setup.py')
-#     with open(setup_path, 'r') as setup_file:
-#         assert 'entry_points' not in setup_file.read()
-#
-
-# def test_bake_with_console_script_files(cookies):
-#     context = {'command_line_interface': 'click'}
-#     result = cookies.bake(extra_context=context)
-#     project_path, project_slug, project_dir = project_info(result)
-#     found_project_files = os.listdir(project_dir)
-#     assert "cli.py" in found_project_files
-#
-#     setup_path = os.path.join(project_path, 'setup.py')
-#     with open(setup_path, 'r') as setup_file:
-#         assert 'entry_points' in setup_file.read()
-
-
-# def test_bake_with_console_script_cli(cookies):
-#     context = {'command_line_interface': 'click'}
-#     result = cookies.bake(extra_context=context)
-#     project_path, project_slug, project_dir = project_info(result)
-#     module_path = os.path.join(project_dir, 'cli.py')
-#     module_name = '.'.join([project_slug, 'cli'])
-#     if sys.version_info >= (3, 5):
-#         spec = importlib.util.spec_from_file_location(module_name, module_path)
-#         cli = importlib.util.module_from_spec(spec)
-#         spec.loader.exec_module(cli)
-#     elif sys.version_info >= (3, 3):
-#         file_loader = importlib.machinery.SourceFileLoader
-#         cli = file_loader(module_name, module_path).load_module()
-#     else:
-#         cli = imp.load_source(module_name, module_path)
-#     runner = CliRunner()
-#     noarg_result = runner.invoke(cli.main)
-#     assert noarg_result.exit_code == 0
-#     noarg_output = ' '.join(['Replace this message by putting your code into', project_slug])
-#     assert noarg_output in noarg_result.output
-#     help_result = runner.invoke(cli.main, ['--help'])
-#     assert help_result.exit_code == 0
-#     assert 'Show this message' in help_result.output
