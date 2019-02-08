40af2f1cef8ae26b54eadf8dabdd712ee11de0d3
Kylen Solvik <kysolvik@gmail.com>
2019-02-08 13:41:50 -0700

    Example tests (#5)
    * Added basic example functions and tests
    
    * Fixed syntax error
    
    * Fixed typo and lint warning
    
    * Fixed typo: max_number not max_numbers
    
    * Commented out bad test function, bc otherwise travis build fails

--- {{cookiecutter.project_slug}}/tests/test_{{cookiecutter.project_slug}}.py
+++ {{cookiecutter.project_slug}}/tests/test_{{cookiecutter.project_slug}}.py
@@ -1,23 +1,38 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
 """Tests for `{{ cookiecutter.project_slug }}` package."""
 
 import pytest
+import random
 
 from {{ cookiecutter.project_slug }} import {{ cookiecutter.project_slug }}
 
+
 @pytest.fixture
-def response():
-    """Sample pytest fixture.
+def generate_numbers():
+    """Sample pytest fixture. Generates list of random integers.
 
     See more at: http://doc.pytest.org/en/latest/fixture.html
     """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+
+    return random.sample(range(100),10)
 
 
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+def test_sum_numbers(generate_numbers):
+    """Sample test function for sum_numbers, using pytest fixture."""
+
+    our_result = {{ cookiecutter.project_slug }}.sum_numbers(generate_numbers)
+    assert our_result == sum(generate_numbers)
+
+
+def test_max_number(generate_numbers):
+    """Sample test function for max_number, using pytest fixture."""
+
+    our_result = {{ cookiecutter.project_slug }}.max_number(generate_numbers)
+    assert our_result == max(generate_numbers)
+
+
+# def test_max_number_bad(generate_numbers):
+#     """Sample test function that fails. Uncomment to see."""
+#
+#     our_result = {{ cookiecutter.project_slug }}.max_number(generate_numbers)
+#     assert our_result == max(generate_numbers) + 1
